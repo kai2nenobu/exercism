a87de5845eefd3019380f3303c76c99dafa107b4
Tsunenobu Kai <kai2nenobu@gmail.com>
2021-01-14 03:54:20 +0900

    [Python] Solve raindrops

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,12 @@
 def convert(number):
-    pass
+    string = ''
+    if number % 3 == 0:
+        string += 'Pling'
+    if number % 5 == 0:
+        string += 'Plang'
+    if number % 7 == 0:
+        string += 'Plong'
+    if len(string) == 0:
+        return str(number)
+    else:
+        return string
