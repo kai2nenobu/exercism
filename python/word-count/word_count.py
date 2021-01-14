cade1708402fe3118a4aa284df6e1a20ae88601e
Tsunenobu Kai <kai2nenobu@gmail.com>
2021-01-14 20:33:21 +0900

    [Python] Solve word-count

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,2 +1,14 @@
-def count_words(sentence):
-    pass
+import re
+
+
+def count_words(sentence: str):
+    lower_sentence = sentence.lower()
+    word_regex = re.compile(r"[a-z0-9]+'?[a-z0-9]*")
+    word_counts = {}
+    for match in word_regex.finditer(lower_sentence):
+        word = re.sub("'$", "", match.group(0))  # Remove a trailing apostrophe
+        if word in word_counts:
+            word_counts[word] += 1
+        else:
+            word_counts[word] = 1
+    return word_counts
