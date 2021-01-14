079774fc2715c37c205b83f14bfe37ae78f2a87f
Tsunenobu Kai <kai2nenobu@gmail.com>
2021-01-14 21:29:49 +0900

    [Python] Solve scrabble-score

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,2 +1,17 @@
-def score(word):
-    pass
+point_to_letters = {
+    1: {'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't'},
+    2: {'d', 'g'},
+    3: {'b', 'c', 'm', 'p'},
+    4: {'f', 'h', 'v', 'w', 'y'},
+    5: {'k'},
+    8: {'j', 'x'},
+    10: {'q', 'z'}
+}
+letter_to_point = {
+    letter: point for point, letters in point_to_letters.items() for letter in letters
+}
+
+
+def score(word: str):
+    return sum([letter_to_point.get(letter, 0) for letter in word.lower()])
+
