bbdc7a7fa46b5b00ab0b5bf0239ab31e248940fd
Tsunenobu Kai <kai2nenobu@gmail.com>
2021-01-14 10:33:13 +0900

    [Python] Solve hamming

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,4 @@
-def distance(strand_a, strand_b):
-    pass
+def distance(strand_a: str, strand_b: str):
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Two strands must have the same length.")
+    return sum([0 if a == b else 1 for a, b in zip(strand_a, strand_b)])
