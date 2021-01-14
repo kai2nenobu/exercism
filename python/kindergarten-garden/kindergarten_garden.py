232af9e0e89b9f1e01457b4a1aa7d7077f2557d2
Tsunenobu Kai <kai2nenobu@gmail.com>
2021-01-14 22:57:55 +0900

    [Python] Solve kindergarten-garden

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,3 +1,60 @@
+from enum import auto, Enum
+
+
+class Plant(Enum):
+    GRASS = auto()
+    CLOVER = auto()
+    RADISHES = auto()
+    VIOLETS = auto()
+
+    @property
+    def title(self):
+        return self.name.title()
+
+    @property
+    def letter(self):
+        return self.name[0]
+
+    @classmethod
+    def from_letter(cls, letter: str):
+        for plant in Plant:
+            if plant.letter == letter:
+                return plant
+
+
 class Garden:
-    def __init__(self, diagram, students):
-        pass
+    __DEFAULT_STUDENTS = [
+        'Alice',
+        'Bob',
+        'Charlie',
+        'David',
+        'Eve',
+        'Fred',
+        'Ginny',
+        'Harriet',
+        'Ileana',
+        'Joseph',
+        'Kincaid',
+        'Larry'
+    ]
+
+    def __init__(self, diagram: str, students=None):
+        diagram_lines = diagram.splitlines()
+        if len(diagram_lines) != 2:
+            raise ValueError('diagrams must be a two-line string')
+        if len(diagram_lines[0]) != len(diagram_lines[1]):
+            raise ValueError('First row and second row of diagram must have the same length.')
+        self.__first_row = diagram_lines[0]
+        self.__second_row = diagram_lines[1]
+        self.__students = Garden.__DEFAULT_STUDENTS if students is None else sorted(students)
+
+    def plants(self, student: str):
+        student_index = self.__students.index(student)
+        return [
+            Plant.from_letter(letter).title for letter in [
+                self.__first_row[student_index * 2],
+                self.__first_row[student_index * 2 + 1],
+                self.__second_row[student_index * 2],
+                self.__second_row[student_index * 2 + 1]
+            ]
+        ]
