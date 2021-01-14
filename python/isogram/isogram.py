1f47dcfb0c4f003eab7617aa84f4e9d6c81aabdb
Tsunenobu Kai <kai2nenobu@gmail.com>
2021-01-14 11:30:35 +0900

    [Python] Solve isogram

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,15 @@
-def is_isogram(string):
-    pass
+def is_isogram(string: str) -> bool:
+    lower_string = string.lower()
+    letter_occurrence = {}
+    for idx, letter in enumerate(lower_string):
+        if letter in letter_occurrence:
+            letter_occurrence[letter].append(idx)
+        else:
+            letter_occurrence[letter] = [idx]
+    for letter, indices in letter_occurrence.items():
+        # Ignore hyphens and spaces
+        if letter in ['-', ' ']:
+            continue
+        if len(indices) > 1:
+            return False
+    return True
